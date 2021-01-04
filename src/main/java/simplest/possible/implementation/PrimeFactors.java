c5daa608ccc584d0d2c95c063cb295b784cc3641
Shubham Pandey <shubhampandey@Shubhams-MacBook-Pro.local>
2021-01-04 18:09:34 +0530

    added solution

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,13 @@
+package simplest.possible.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+    public <T> List<Integer> of(int number) {
+        if(number == 1) return new ArrayList<>();
+        // left for all the rest of numbers
+        // because for them test has not been created yet.
+        return null;
+    }
+}
